a7b4cd2250695fbf54d017fed1ba2c6472919a47
Abdganiyy <kelani.abdganiyy1@gmail.com>
2024-10-02 13:26:34 +0100

    word correction

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,7 +10,7 @@ function Skills() {
     <div className='section-container'>
       <Header
         heading="My Skills."
-        subHeading="Passionate about new technologies, I keep exploring stuff. Here's the tech stack I've worked with!"></Header>
+        subHeading="Passionate about new technologies, I keep exploring stuff. Here are the technologies I've worked with:"></Header>
       <div className='skill-card-container'>
       {
         skillList.map(({skillName, skillUrl}, index) =>  <SkillCard key={index} skillName={skillName}
